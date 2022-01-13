924680b5ee566e9cf11db8f57ac8b097db337c0f
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-13 23:50:25 +0300

    added logic to app.js

--- src/components/CurrencyRow.jsx
+++ src/components/CurrencyRow.jsx
@@ -1,9 +1,28 @@
-export const CurrencyRow = () => {
+const { v4: uuidv4 } = require("uuid");
+
+export const CurrencyRow = (props) => {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount,
+  } = props;
+
   return (
     <div>
-      <input type="number" />
-      <select>
-        <option value="some value">some value</option>
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={uuidv4()} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </div>
   );
